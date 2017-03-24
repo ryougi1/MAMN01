92d26f49fdffd1e58b8d817dda22ef6af220e6b4
ryougi1 <shplankey@live.se>
2017-03-24 15:23:45 +0100

    Added some compass functionality using hardware sensors.

--- app/src/main/java/com/example/tutorial/MainActivity.java
+++ app/src/main/java/com/example/tutorial/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.tutorial;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void bStart(View view) {
+        Intent intent = new Intent(getApplicationContext(), CompassActivity.class);
+        startActivity(intent);
+    }
+
 }
